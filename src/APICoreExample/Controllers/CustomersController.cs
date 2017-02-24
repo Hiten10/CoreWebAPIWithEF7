846df5db6f3e9924d1ae00ee4c4fc68149f03949
Hitendra Kumar <kumar.hiten@hotmail.com>
2017-02-24 21:57:21 +0530

    Add project files.

---
+++ src/APICoreExample/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using APICoreExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICoreExample.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomersController:Controller
+    {
+        TestDBContext _db = new TestDBContext();
+        public async Task<IActionResult> Get()
+        {
+            var result = await _db.Customer.ToAsyncEnumerable().ToList();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/APICoreExample/Models/JobInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APICoreExample.Models
+{
+    public partial class JobInfo
+    {
+        public int JobId { get; set; }
+        public string Name { get; set; }
+        public DateTime LastExecutionDate { get; set; }
+        public string Status { get; set; }
+    }
+}
